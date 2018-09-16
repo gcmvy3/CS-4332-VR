938c73d580161e252ffffc9d97b4ae9402fb46db
BobsCandyCanes <grantmatthews28@yahoo.com>
2018-09-15 21:31:58 -0500

    added navmesh

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NewtonVR;
+
+public class InputTest : MonoBehaviour {
+
+    NVRPlayer player;
+    NVRHand rightHand;
+    NVRHand leftHand;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindObjectOfType<NVRPlayer>();
+        rightHand = player.RightHand;
+        leftHand = player.LeftHand;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log("Updating");
+
+		if(rightHand.HoldButtonDown) {
+            Debug.Log("Right hand hold");
+        }
+	}
+}
